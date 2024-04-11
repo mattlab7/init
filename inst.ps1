9996c914143b9d0fd278dc49c84c75fe0eca5812
Matt <43501106+mattlab7@users.noreply.github.com>
2024-04-12 00:33:29 +0800

    Update inst.ps1

--- inst.ps1
+++ inst.ps1
@@ -1,72 +1,71 @@
 param(
-	[Parameter(Mandatory=$true)]
-	[string] $Hostname
+    [Parameter(Mandatory=$true)]
+    [string]$Hostname
 )
 
 $appGroups = @{
-	sapphire = @(
-		"7zip.7zip", # Utilities
-		"ShareX.ShareX",
-		"QL-Win.QuickLook",
-		"Google.Chrome.Dev",
-		"AgileBits.1Password",
-		"Tailscale.Tailscale",
-		"AppWork.JDownloader",
-		"Soulseek.SoulseekQt",
-		"OBSProject.OBSStudio",
-		"Tixati.Tixati.Portable",
-		"Google.ChromeRemoteDesktop",
-		"mpv.net", # Media
-		"Gyan.FFmpeg"
-		"Stremio.Stremio",
-		"DuongDieuPhap.ImageGlass",
-		"Bruno.Bruno", # Dev Tools
-		"Canonical.Ubuntu.2204",
-		"Microsoft.WindowsTerminal",
-		"Microsoft.VisualStudioCode",
-		"JetBrains.IntelliJIDEA.Ultimate",
-		"Microsoft.VisualStudio.2022.Community.Preview",
-		"9NKSQGP7F2NH", #Communication (WhatsApp)
-		"Discord.Discord",
-		"Telegram.TelegramDesktop",
-		"Valve.Steam", # Gaming
-		"PrismLauncher.PrismLauncher",
-		"EpicGames.EpicGamesLauncher"
-	),
-	quartz = @(
-		"7zip.7zip", # Utilities
-		"ShareX.ShareX",
-		"QL-Win.QuickLook",
-		"Google.Chrome.Dev",
-		"AgileBits.1Password",
-		"Tailscale.Tailscale",
-		"OBSProject.OBSStudio",
-		"Google.ChromeRemoteDesktop",
-		"mpv.net", # Media
-		"Gyan.FFmpeg"
-		"Stremio.Stremio",
-		"DuongDieuPhap.ImageGlass",
-		"Bruno.Bruno", # Dev Tools
-		"Canonical.Ubuntu.2204",
-		"Microsoft.WindowsTerminal",
-		"Microsoft.VisualStudioCode",
-		"JetBrains.IntelliJIDEA.Ultimate",
-		"Microsoft.VisualStudio.2022.Community.Preview",
-		"9NKSQGP7F2NH", #Communication (WhatsApp)
-		"Telegram.TelegramDesktop"
-	)
+    sapphire = @(
+        "7zip.7zip", # utils
+        "ShareX.ShareX",
+        "QL-Win.QuickLook",
+        "Google.Chrome.Dev",
+        "AgileBits.1Password",
+        "Tailscale.Tailscale",
+        "AppWork.JDownloader",
+        "Soulseek.SoulseekQt",
+        "OBSProject.OBSStudio",
+        "Tixati.Tixati.Portable",
+        "Google.ChromeRemoteDesktop",
+        "mpv.net", # media
+        "Gyan.FFmpeg",
+        "Stremio.Stremio",
+        "DuongDieuPhap.ImageGlass",
+        "Bruno.Bruno", # dev tools
+        "Canonical.Ubuntu.2204",
+        "Microsoft.WindowsTerminal",
+        "Microsoft.VisualStudioCode",
+        "JetBrains.IntelliJIDEA.Ultimate",
+        "Microsoft.VisualStudio.2022.Community.Preview",
+        "9NKSQGP7F2NH", # comms (whatsapp)
+        "Discord.Discord",
+        "Telegram.TelegramDesktop",
+        "Valve.Steam", # games
+        "PrismLauncher.PrismLauncher",
+        "EpicGames.EpicGamesLauncher"
+    )
+    quartz = @(
+        "7zip.7zip", # utils
+        "ShareX.ShareX",
+        "QL-Win.QuickLook",
+        "Google.Chrome.Dev",
+        "AgileBits.1Password",
+        "Tailscale.Tailscale",
+        "OBSProject.OBSStudio",
+        "Google.ChromeRemoteDesktop",
+        "mpv.net", # media
+        "Gyan.FFmpeg",
+        "Stremio.Stremio",
+        "DuongDieuPhap.ImageGlass",
+        "Bruno.Bruno", # dev tools
+        "Canonical.Ubuntu.2204",
+        "Microsoft.WindowsTerminal",
+        "Microsoft.VisualStudioCode",
+        "JetBrains.IntelliJIDEA.Ultimate",
+        "Microsoft.VisualStudio.2022.Community.Preview",
+        "9NKSQGP7F2NH", # comms (whatsapp)
+        "Telegram.TelegramDesktop"
+    )
 }
 
 if ($appGroups.ContainsKey($Hostname)) {
-	$appsToInstall = $appGroups[$Hostname]
-	Write-Host "Installing applications from group $($Hostname):"
+    $appsToInstall = $appGroups[$Hostname]
+    Write-Host "Installing apps for $Hostname..."
 
-	foreach ($appId in $appsToInstall) {
-		Write-Host "  - $($appId)"
-		winget install --id=$appId -e
-	}
-} else {
-	Write-Error "Invalid group name: $($Hostname)"
+    foreach ($appId in $appsToInstall) {
+        Write-Host "Installing $appId"
+        winget install --id $appId --accept-source-agreements --accept-package-agreements
+    }
 }
-
-Write-Host "Script completed."
+else {
+    Write-Error "Invalid hostname provided. Must be 'sapphire' or 'quartz'."
+}
